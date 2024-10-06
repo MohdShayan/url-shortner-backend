66cd9495351807771a389c0f3ca1917292f95084
Mohd. Shayan <70843731+MohdShayan@users.noreply.github.com>
2024-10-06 17:55:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,15 @@ import urlRoutes from './routes/urlRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://url-shortner-frontend-liard.vercel.app', // replace with your frontend URL
-  credentials: true // if you are sending cookies or authorization headers
-}));
+
+ConnectDB(); 
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Connect to the database once, when handling requests
-app.use((req, res, next) => {
-    ConnectDB(); 
-    next();
-});
-
+// Routes
 app.use('/api/url', urlRoutes);
 
 // Export the app for Vercel
