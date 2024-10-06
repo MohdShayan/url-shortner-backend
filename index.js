96a963a23cfcb1c3947b3be0d176345fc5a6e9fe
Mohd. Shayan <70843731+MohdShayan@users.noreply.github.com>
2024-10-06 17:32:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-import express, { urlencoded } from 'express'
-import dotenv from "dotenv"
-import cors from "cors"
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import { ConnectDB } from './connection/connnectDB.js';
-import urlRoutes from './routes/urlRoutes.js'
+import urlRoutes from './routes/urlRoutes.js';
 
-const app= express();
-app.use(cors());
 dotenv.config();
 
-const PORT= process.env.PORT || 5001
-
+const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/url',urlRoutes);
+// Connect to the database once, when handling requests
+app.use((req, res, next) => {
+    ConnectDB(); 
+    next();
+});
 
-app.listen(PORT,()=>{
-    ConnectDB();
-    console.log(`Server Started at http://localhost:${PORT}`);
-})
+app.use('/api/url', urlRoutes);
+
+// Export the app for Vercel
+export default app;
